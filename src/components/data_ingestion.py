d2b5e8000a067eab9b90da5b54d2e5f632fd92fb
ANSH BIRE <bireansh1@gmail.com>
2026-05-27 13:55:39 +0530

    updated comment

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -36,7 +36,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')        # can get data from Databases (mongoDB), etc.. 
             logging.info('Read the Dataset as dataframe')
             
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)  # creates the directory artifact/...
