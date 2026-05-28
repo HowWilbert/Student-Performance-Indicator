ecfe33cbfc41597390636ff85f58b37dbb2b27ff
ANSH BIRE <bireansh1@gmail.com>
2026-05-28 15:16:45 +0530

    removed artifact folder

--- application.py
+++ application.py
@@ -1,42 +1,43 @@
-from flask import Flask, request, render_template
+from flask import Flask,request,render_template
 import pandas as pd
+import traceback
 
-from src.pipeline.predict_pipeline import CustomData, PredictPipeline
-
-application = Flask(__name__)
-
-app = application
-
+app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home_page():
     return render_template('index.html')
 
 
-@app.route('/predictdata', methods=['GET', 'POST'])
+@app.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
+    try:
+        if request.method=='GET':
+            return render_template('home.html')
 
-    if request.method == 'GET':
-        return render_template('home.html')
+        else:
+            data = {
+                "gender":[request.form.get('gender')],
+                "race_ethnicity":[request.form.get('ethnicity')],
+                "parental_level_of_education":[request.form.get('parental_level_of_education')],
+                "lunch":[request.form.get('lunch')],
+                "test_preparation_course":[request.form.get('test_preparation_course')],
+                "reading_score":[float(request.form.get('reading_score'))],
+                "writing_score":[float(request.form.get('writing_score'))]
+            }
 
-    else:
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
-        )
+            pred_df = pd.DataFrame(data)
 
-        pred_df = data.get_data_as_data_frame()
+            from src.pipeline.predict_pipeline import PredictPipeline
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
 
-        return render_template('home.html', results=results[0])
+            return render_template('home.html',results=results[0])
+
+    except Exception as e:
+        return f"<h1>ERROR</h1><pre>{traceback.format_exc()}</pre>"
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(host="0.0.0.0")
