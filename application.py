47a35007837fd2c0605fcaa8cf2c663cc976b0c1
ANSH BIRE <bireansh1@gmail.com>
2026-05-28 14:40:40 +0530

    fixed flask app and prediction route

--- application.py
+++ application.py
@@ -1,18 +1,19 @@
-from flask import Flask,request,render_template
-import numpy as np
+from flask import Flask, request, render_template
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
-from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 
-## Route for a home page
-@application.route('/')
+app = application
+
+
+@app.route('/')
 def index():
     return render_template('index.html')
 
-@application.route('/predictdata', methods=['GET','POST'])
+
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
 
     if request.method == 'GET':
@@ -35,3 +36,7 @@ def predict_datapoint():
         results = predict_pipeline.predict(pred_df)
 
         return render_template('home.html', results=results[0])
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
